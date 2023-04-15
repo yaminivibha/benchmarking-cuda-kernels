f8fce84ec512f286f179f529b5ac5f341e0d5b0c
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-15 19:22:47 -0400

    adding problem 2

---
+++ CUDA1/vecaddKernel01.cu
@@ -0,0 +1,11 @@
+
+
+__global__ void AddVectors(const float* A, const float* B, float* C, int N)
+{
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    int stride = blockDim.x * gridDim.x;
+
+    for (int i = tid; i < N; i += stride) {
+        C[i] = A[i] + B[i];
+    }
+}
