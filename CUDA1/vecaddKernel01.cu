108bf0b3105369159197a6b9f5a18aaf8df73364
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-15 19:44:11 -0400

    bug squash

--- CUDA1/vecaddKernel01.cu
+++ CUDA1/vecaddKernel01.cu
@@ -3,7 +3,7 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    while (tid < n) {
+    while (tid < N) {
         C[tid] = A[tid] + B[tid];
         tid += blockDim.x * gridDim.x;
     }
