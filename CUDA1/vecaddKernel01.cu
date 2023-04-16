46ad08412573b25632b9330406ebdefb65af7e80
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-16 08:47:56 -0400

    cleaning up prints in kernel

--- CUDA1/vecaddKernel01.cu
+++ CUDA1/vecaddKernel01.cu
@@ -3,14 +3,9 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
-    int threadEndIndex   = threadStartIndex + N;
-    printf("tid = %d\n", tid);
+    int threadEndIndex   = tid + N;
     while (tid < threadEndIndex) {
         C[tid] = A[tid] + B[tid];
-        printf("C[tid] = %f\n", C[tid]);
-        printf("A[tid] = %f\n", A[tid]);
-        printf("B[tid] = %f\n", B[tid]); 
         tid = tid + blockDim.x * gridDim.x;
-        printf("next tid = %d\n", tid);
     }
 }
