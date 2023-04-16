1f629214c38d5f9b682a45ed61f178820c300ec5
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-15 20:37:25 -0400

    updating with test on p2

--- Problem2/KMillionAddition.cc
+++ Problem2/KMillionAddition.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <cstdlib> 
+#include <cmath>
 
 using namespace std;
 
@@ -14,13 +15,14 @@ int main(int argc, char* argv[]) {
   const int K_million = K * 1000000; 
   int* arr1 = new int[K_million];
   int* arr2 = new int[K_million];
+  int* result = new int[K_million];
+
   for (int i = 0; i < K_million; i++) {
     arr1[i] = (float)i;
     arr2[i] = (float)(K_million-i);   
   }
   // timing only the addition
   auto start_time = chrono::steady_clock::now();
-  int* result = new int[K_million];
   for (int i = 0; i < K_million; i++) {
     result[i] = arr1[i] + arr2[i];
   }
@@ -31,12 +33,12 @@ int main(int argc, char* argv[]) {
   cout << "Runtime: " << duration << " ms" << endl;
   
   // Verify & report result
-    for (i = 0; i < N; ++i) {
-        float val = h_C[i];
-        if (fabs(val - N) > 1e-5)
+    for (int i = 0; i < K_million; ++i) {
+        float val = result[i];
+        if (fabs(val - K_million) > 1e-5)
             break;
     }
-    if (i == N)
+    if (i == K_million)
         printf("PASSED\n");
     else
         printf("FAILED\n");
