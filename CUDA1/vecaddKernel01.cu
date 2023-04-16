9172afa75b514e5daa634719bd459771dd2ad198
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-16 12:15:10 -0400

    commenting the kernel

--- CUDA1/vecaddKernel01.cu
+++ CUDA1/vecaddKernel01.cu
@@ -1,9 +1,12 @@
+#include <stdio.h>
+
 __global__ void vectorSum(const float *A, const float *B, float *C, int size) {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
     int chunk_size = blockDim.x;
     int chunk_id = blockIdx.x;
 
     for (int i = chunk_id * chunk_size + threadIdx.x; i < size; i += chunk_size * gridDim.x) {
+        printf("tid: %d, i: %d\n", tid, i)
         C[i] = A[i] + B[i];
     }
 }
