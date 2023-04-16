74f89679f43f16cc65802d9e71b225655c86d754
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-16 08:26:35 -0400

    trying one more kernel

--- CUDA1/vecaddKernel01.cu
+++ CUDA1/vecaddKernel01.cu
@@ -6,9 +6,9 @@ __global__ void AddVectors(const float* A, const float* B, float* C, int N)
     int threadStartIndex = blockStartIndex + (threadIdx.x * N);
     int threadEndIndex   = threadStartIndex + N;
     int i;
-    int stride = blockDim.x * gridDim.x;
+    int stride = blockDim.x;
     
-    for (i = threadStartIndex; i < N; i += stride) {
+    for (i = threadStartIndex; i < threadEndIndex; i += stride) {
         C[i] = A[i] + B[i];
     }
 }
