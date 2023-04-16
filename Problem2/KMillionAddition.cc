1a0e3e24ca9280995eb80090b3953e66b4968eed
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-15 20:25:02 -0400

    adding problem2

---
+++ Problem2/KMillionAddition.cc
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <chrono>
+#include <cstdlib> 
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  // correcting usage
+  if (argc != 2) {
+    cerr << "Usage: " << argv[0] << " K" << endl;
+    return 1;
+  }
+  const int K = stoi(argv[1]);
+  const int K_million = K * 1000000; 
+  int* arr1 = new int[K_million];
+  int* arr2 = new int[K_million];
+  for (int i = 0; i < K_million; i++) {
+    arr1[i] = rand();
+    arr2[i] = rand();
+  }
+  // timing only the addition
+  auto start_time = chrono::steady_clock::now();
+  int* result = new int[K_million];
+  for (int i = 0; i < K_million; i++) {
+    result[i] = arr1[i] + arr2[i];
+  }
+  auto end_time = chrono::steady_clock::now();
+  auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+  
+  // printing result
+  cout << "Runtime: " << duration << " ms" << endl;
+  
+  // freeing memory
+  free(arr1);
+  free(arr2);
+  free(result);
+  return 0;
+}
