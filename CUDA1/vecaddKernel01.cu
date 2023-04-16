a85c6c601f3a84f09fd56f9d919fc7574e76904a
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-16 08:42:49 -0400

    trying a small update

--- CUDA1/vecaddKernel01.cu
+++ CUDA1/vecaddKernel01.cu
@@ -3,8 +3,13 @@
 __global__ void AddVectors(const float* A, const float* B, float* C, int N)
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("tid = %d\n", tid);
     while (tid < N) {
         C[tid] = A[tid] + B[tid];
+        printf("C[tid] = %f\n", C[tid]);
+        printf("A[tid] = %f\n", A[tid]);
+        printf("B[tid] = %f\n", B[tid]); 
         tid = tid + blockDim.x * gridDim.x;
+        printf("next tid = %d\n", tid);
     }
 }
