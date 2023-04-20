44d8473b32c7e5f39ef02f06afcb2300ca958be6
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-04-19 21:15:52 -0400

    fixing ...

--- Problem3/Convolution.cu
+++ Problem3/Convolution.cu
@@ -39,57 +39,6 @@ typedef struct {
   double* elements;
 } Matrix;
 
-// Function declarations
-__global__ void ConvKernel(const Matrix, const Matrix*, Matrix);
-Matrix MakeDeviceMatrix(const Matrix M, bool copy);
-Matrix* MakeDeviceFilters(const Matrix* filters, bool copy);
-Matrix MakeHostMatrix(int channels, int height, int width);
-Matrix createIMatrix();
-Matrix* createFilterMatrices() 
-Matrix createResultMatrix();
-Matrix createIMatrix();
-double checkResult(Matrix result);
-
-__global__ void ConvKernel(const Matrix input_matrix, const Matrix* filters, Matrix result){
-  int block_row = blockIdx.x;
-  int block_col = blockIdx.y;
-  int k = blockIdx.z;
-
-  int x = threadIdx.x;
-  int y = threadIdx.y;
-
-  Matrix filter = filters[k];
-
-  double* resultSub = &result.elements[block_row * OUT_FOOTPRINT * result.stride_height + block_col * OUT_FOOTPRINT];
-  double* input_matrixSub = &input_matrix.elements[block_row * OUT_FOOTPRINT * input_matrix.stride_height + block_col * OUT_FOOTPRINT];
-  
-  __shared__ double shared_input_matrix[C][OUT_FOOTPRINT + 2 * P][OUT_FOOTPRINT + 2 * P];
-
-  #pragma unroll
-  for(int c = 0; c < C; c++) {
-    shared_input_matrix[c][x][y] = input_matrixSub[c * input_matrix.stride_channel + x * input_matrix.stride_height + y];
-  }
-
-  __syncthreads();
-
-  if(x < P || x >= OUT_FOOTPRINT + P || y < P || y >= OUT_FOOTPRINT + P) {
-    return;
-  }
-
-  int new_x = x - P;
-  int new_y = y - P;
-
-  double result_val = 0;
-  for(int c = 0; c < C; c++) {
-    for(int j = 0; j < FH; j++) {
-      for(int i = 0; i < FW; i++) {
-        result_val += filter.elements[c * filter.stride_channel + (FH - 1 - i) * filter.stride_height + (FW - 1 - j)] * shared_input_matrix[c][new_x + i][new_y + j];
-      }
-    }
-  }
-
-  resultSub[k * result.stride_channel + new_x * result.stride_height + new_y] = result_val;
-}
 
 
 Matrix MakeDeviceMatrix(const Matrix M, bool copy){
@@ -169,9 +118,9 @@ Matrix* createFilterMatrices() {
 }
 
 // Compute and check the checksum of the result matrix
-// expected to be 122756344698240
 double checkResult(Matrix M) {
 
+  // expected result
   double checksum = 122756344698240;
   double checksum_M = 0;
   
@@ -188,7 +137,7 @@ double checkResult(Matrix M) {
     printf("computed checksum: %lf\n", checksum_M);
     printf("actual checksum: %lf\n", checksum);
   }
-    return 122756344698240;
+    return checksum_M;
 }
 
 // Host code for convolution.
@@ -241,10 +190,6 @@ void Conv(const Matrix input_matrix, const Matrix* filters, Matrix result){
   cudaFree(filters_device);
   cudaFree(result_device.elements);
 }
-
-//
-// main
-
 
 
 int main() {
